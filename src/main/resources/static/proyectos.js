f3270cc7573a9af542a75cfb563b01a907b473ef
Julian Devia <julian.devia@mail.escuelaing.edu.co>
2016-10-31 23:17:53 -0500

    funciona la vista de proyectos, aún no hace push

--- src/main/resources/static/proyectos.js
+++ src/main/resources/static/proyectos.js
@@ -1,64 +1,87 @@
-var botonAcceder='<button type="submit" class="mui-btn mui-btn--raised" onclick=""><i class="fa fa-rocket"></i>Acceder</button>';
-var radioButtonIn='<input type="radio" name="proyecto" value="';
-var radioButtonFin='" checked> Seleccionar<br>';
-var formIn='<form id="radio">';
-var formFin= '</form>';
+var botonAcceder = '<button type="submit" class="mui-btn mui-btn--raised" onclick=""><i class="fa fa-rocket"></i>Acceder</button>';
+var radioButtonIn = '<input type="radio" name="proyecto" value="';
+var radioButtonFin = '" checked> Seleccionar<br>';
+var formIn = '<form id="radio">';
+var formFin = '</form>';
 var proyecto = null;
-var proyectos=0;
-var arrP={};
-var connected=null;
+var proyectos = 0;
+var arrP = [];
 var stompClient = null;
-function formAgregarProyecto(){
+
+function formAgregarProyecto() {
     $("#newP").show();
 };
 
-function agregarProyecto(){
+function inicio(){
+    $("#usrnm").html("Usuario: " + sessionStorage.name);
+    disconnect();
+    connect();
+    getProyectos();
+}
+function agregarProyecto() {
     console.log($("#nomP").val());
     console.log($("#descP").val());
     $("#newP").hide();
-    proyecto=new Proyecto($("#nomP").val(),$("#descP").val());
+    proyecto = new Proyecto($("#nomP").val(), $("#descP").val());
+    //postProyecto(proyecto);
     sendProject();
 };
 
-function agregarProyectoVista(proy){
+function agregarProyectoVista(proy) {
     proyectos++;
-    arrP[proy.nombre]=proy;
-    $("#lista").append("<tr><td>"+proy.nombre+"</td><td>"+proy.descripcion+"</td><td>"+proy.fechaCreacion.toLocaleString()+
-    "</td><td>"+proy.fechaUltimaModificacion.toLocaleString()+"</td><td>"+botonAcceder+"</td><td>"+radioButtonIn+proy.nombre+radioButtonFin+"</td>"+"</tr>");
-    console.log("</td><td>"+radioButtonIn+proy.nombre+radioButtonFin+"</td>");
+    arrP[proy.nombre] = proy;
+    $("#lista").append("<tr><td>" + proy.nombre + "</td><td>" + proy.descripcion + "</td><td>" + proy.fechaCreacion.toLocaleString() +
+        "</td><td>" + proy.fechaUltimaModificacion.toLocaleString() + "</td><td>" + botonAcceder + "</td><td>" + radioButtonIn + proy.nombre + radioButtonFin + "</td>" + "</tr>");
 };
 
-sendProject = function () {
-    stompClient.send('/app/newproject.'+name, {}, JSON.stringify(proyecto));
+sendProject = function() {
+    stompClient.send('/app/newproject.' + sessionStorage.name, {}, JSON.stringify(proyecto));
 };
 
 function connect() {
-    connected=true;
-    var socket = new SockJS('/stompendpoint');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/newproject.'+sessionStorage.name, function (data) {
-            console.log("llego");
-            var objeto = JSON.parse(data.body);
-            objeto.fechaCreacion= new Date(objeto.fechaCreacion);
-            objeto.fechaUltimaModificacion= new Date(objeto.fechaUltimaModificacion);
-            agregarProyectoVista(objeto);
+    if (sessionStorage.connected!=true) {
+        sessionStorage.connected = true;
+        var socket = new SockJS('/stompendpoint');
+        stompClient = Stomp.over(socket);
+        stompClient.connect({}, function(frame) {
+            console.log('Connected: ' + frame);
+            stompClient.subscribe('/topic/newproject.' + sessionStorage.name, function(data) {
+                console.log("llego");
+                var objeto = JSON.parse(data.body);
+                objeto.fechaCreacion = new Date(objeto.fechaCreacion);
+                objeto.fechaUltimaModificacion = new Date(objeto.fechaUltimaModificacion);
+                agregarProyectoVista(objeto);
+            });
         });
-    });
+    }
 };
 
 function disconnect() {
-    connected=false;
-    if (stompClient != null) {
-        sessionStorage.stompClient.disconnect();
-    }
-    console.log("Disconnected");
-};
-$(document).ready(
-        function () {
-            $("#usrnm").html("Usuario: "+sessionStorage.name);
-            connect();
-
+        sessionStorage.connected = false;
+        if (stompClient != null) {
+            sessionStorage.stompClient.disconnect();
         }
+        console.log("Disconnected");
+};
+
+function getProyectos(){
+    return $.get("/projects/users/"+sessionStorage.name,function(data){
+        console.log(data);
+        for(element in data){
+            data[element].fechaCreacion = new Date(data[element].fechaCreacion);
+            data[element].fechaUltimaModificacion = new Date(data[element].fechaUltimaModificacion);
+            agregarProyectoVista(data[element]);
+        };
+    });
+}
+
+function postProyecto(proyecto){
+    console.log(JSON.stringify(proyecto));
+    console.log("plain? "+jQuery.isPlainObject(JSON.stringify(proyecto)));
+    return $.post("/projects/users/"+sessionStorage.name,proyecto);
+}
+$(document).ready(
+    function() {
+        inicio();
+    }
 );
