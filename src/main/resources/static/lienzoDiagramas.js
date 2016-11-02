dbe29f6314c196b6f51a84da00cb6e6097eec2d6
Julian Devia <julian.devia@mail.escuelaing.edu.co>
2016-11-02 01:43:51 -0500

    ya agrega cajas con nombres

--- src/main/resources/static/lienzoDiagramas.js
+++ src/main/resources/static/lienzoDiagramas.js
@@ -2,6 +2,7 @@
 stompClient = null;
 isConnect = false;
 elementos = [];
+elementosUsados=[];
 /**/
 
 /**
@@ -59,23 +60,46 @@ actualizarVistaElementos = function () {
 inicio = function () {
     $("#infousuario").html("<center>Usuario:" + sessionStorage.name + "</center><center>Nombre del proyecto:" + sessionStorage.nameProject + "</center>");
     $("#tituloDiagrama").html(sessionStorage.nameDiagram);
+    var w = window.innerWidth*.6;
+    var h = window.innerHeight*.8;
+    $("#canvasdiv").html('<canvas id="lienzo" width="'+w+'px" height="'+h+'px"></canvas>');
 };
+
+function canvasDroppable(){
+    $("#lienzo").droppable({
+        accept: "li",
+        drop: function (event, ui) {
+            var texto=$(ui.draggable).clone().text();
+            console.log(elementosUsados[texto]);
+            if(typeof elementosUsados[texto] == "undefined"){
+                elementosUsados[texto]=[];
+            }
+            elementosUsados[texto].push(texto+elementosUsados[texto].length);
+            texto+=elementosUsados[texto].length;
+            $('#lienzo').drawRect({
+                fillStyle: '#afb4b7',
+                x: event.clientX-event.target.offsetLeft, y: event.clientY-event.target.offsetTop,
+                width: 100,
+                height: 50
+            });
+            $('#lienzo').drawText({
+                fillStyle: '#fff',
+                strokeStyle: '#000',
+                x: event.clientX-event.target.offsetLeft, y: event.clientY-event.target.offsetTop,
+                fontSize: 12,
+                fontFamily: 'Verdana, sans-serif',
+                text: texto
+            });
+            /*var context = $(this)[0].getContext("2d");
+            context.font = "16px helvetica";
+            context.fillText($(ui.draggable).clone().text(), ui.position.left - event.target.offsetLeft, ui.position.top - event.target.offsetTop);*/
+        }
+    });
+}
 $(document).ready(
         function () {
             inicio();
             actualizarElementos();
-            $("#lienzo").droppable({
-                accept: "li",
-                drop: function (event, ui) {
-                    var context = $(this)[0].getContext("2d");
-                    context.font = "16px helvetica";
-                    context.fillText($(ui.draggable).clone().text(), ui.position.left - event.target.offsetLeft, ui.position.top - event.target.offsetTop);
-                }
-            });
-
-
+            canvasDroppable();
         }
 );
-
-
-
